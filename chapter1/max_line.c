7e0f4bb0d9914b9b9c600d845897c414c2ca68f8
huhuchen <chengengtiger@gmail.com>
2012-08-30 22:38:28 +0800

    use extern to refactor max_line

--- chapter1/max_line.c
+++ chapter1/max_line.c
@@ -1,53 +1,108 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-int get_line(char line[], int limit);
-void copy(char to[], char from[]);
+//int get_line(char line[], int limit);
+//void copy(char to[], char from[]);
+//
+//int main()
+//{
+//    int max, len;
+//    char line[MAXLINE];
+//    char longest[MAXLINE];    
+//    
+//    max = 0;
+//
+//    while ((len = get_line(line, MAXLINE)) > 0)
+//    {
+//        printf("\n len is :%d", len);
+//        if (len > max){
+//            max = len;
+//            copy(longest, line);
+//        }
+//    }
+//
+//    printf("\n the logest line is : %d \n", max);
+//
+//    return 0;
+//}
+//
+//int get_line(char s[], int limit)
+//{
+//    int i, c;
+// 
+//    for (i = 0; i < limit && (c = getchar()) != EOF && c != '\n'; ++i)
+//        s[i] = c;
+//
+//    if(c == '\n'){
+//        s[i] = c;
+//        ++i;
+//    }
+//    s[i] = '\0';
+//    return i;
+//}
+//
+//void copy(char to[], char from[])
+//{
+//    int i;
+//    for(i = 0; i != '\0';++i)
+//    {
+//        to[i] = from[i];
+//    }
+//}
+//
+//
+//
+
+
+
+int max;
+char line[MAXLINE], longest[MAXLINE];
+
+int get_line(void);
+void copy(void);
 
 int main()
 {
-    int max, len;
-    char line[MAXLINE];
-    char longest[MAXLINE];    
-    
-    max = 0;
+    int len;
+    extern int max;
+    extern char longest[];
 
-    while ((len = get_line(line, MAXLINE)) > 0)
+    while ((len = get_line()) > 0)
     {
-        printf("\n len is :%d", len);
-        if (len > max){
+        if(len > max){
             max = len;
-            copy(longest, line);
+            copy();
         }
     }
-
-    printf("\n the logest line is : %d \n", max);
-
+    printf("\nthe length of longest line is: %d\n", max);    
+    printf("\nthe longest line is \n%s", longest);
     return 0;
 }
 
-int get_line(char s[], int limit)
-{
-    int i, c;
- 
-    for (i = 0; i < limit && (c = getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
 
-    if(c == '\n'){
-        s[i] = c;
+int get_line(void)
+{
+    int i, c; 
+    extern int max;
+    extern char line[];
+
+    for (i = 0; i < MAXLINE && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+
+    if (c == '\n'){
+        line[i] = c;
         ++i;
     }
-    s[i] = '\0';
+    line[i] = '\0';
+
     return i;
 }
 
-void copy(char to[], char from[])
+
+void copy(void)
 {
-    int i;
-    for(i = 0; i != '\0';++i)
-    {
-        to[i] = from[i];
-    }
+    int i = 0;
+    extern char line[], longest[];
+    while ((longest[i] = line[i]) != '\0')
+        ++i;
 }
-
-
